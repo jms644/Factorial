2f89bbd60c5998052d5affae432d318235f78418
Joshua Schwartz <Josh@Joshuas-MacBook-Pro.local>
2014-08-10 11:06:25 -0700

    Initial Commit

---
+++ Factorial/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Factorial
+//
+//  Created by Joshua Schwartz on 8/10/14.
+//  Copyright (c) 2014 JOSH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
